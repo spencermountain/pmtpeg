aa796af00d16842526084ff79256f36445e996f1
Spencer Kelly <spencermountain@gmail.com>
2026-09-23 10:17:39 -0400

    linting

--- test/fromUrl.test.js
+++ test/fromUrl.test.js
@@ -7,7 +7,7 @@ const VANCOUVER = 'https://snip.spencermountain.dev/2025/maps/vancouver.pmtiles'
 
 test('fromUrl() reads a header over HTTP range requests', {}, async () => {
   const pm = fromUrl(VANCOUVER);
-  let header = await pm.header();
+  const header = await pm.header();
   assert.equal(header.tileType, 1);
   assert.equal(header.tileTypeName, 'mvt');
   assert.equal(header.tileCompression, 2);
